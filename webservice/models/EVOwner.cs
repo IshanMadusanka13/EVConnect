8fbf12c76efdece02f8acb1950ef29532035a17d
Ishan Madusanka <madusankaishan13@gmail.com>
2025-09-28 13:58:00 +0530

    Changed All Models

--- webservice/models/EVOwner.cs
+++ webservice/models/EVOwner.cs
@@ -9,14 +9,21 @@ namespace webservice.models
     public class EVOwner
     {
         public string NIC { get; set; }
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
         public string Email { get; set; }
-        public string ContactNumber { get; set; }
-        public string VehicleNumber { get; set; }
-        public string VehicleType { get; set; }
-        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
         public string Address { get; set; }
+        public string Password { get; set; }
+        public string VehicleType { get; set; } // "Car", "Bike"
+        public string VehicleModel { get; set; }
+        public string VehiclePlateNumber { get; set; }
+        public string BatteryCapacity { get; set; }
+        public string CompatibleChargerTypes { get; set; } // "AC,DC,Super"
         public bool IsActive { get; set; }
+        public DateTime RegistrationDate { get; set; }
 
     }
 
--- webservice/models/User.cs
+++ webservice/models/User.cs
@@ -8,12 +8,13 @@ namespace webservice.models
 {
     public class User
     {
-        public string Id { get; set; }
-        public string Username { get; set; }
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
-        public string PhoneNumber { get; set; }
         public bool IsActive { get; set; }
 
     }
