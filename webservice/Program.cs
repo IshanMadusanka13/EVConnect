36d455bcdbee6fadd8febcc6efb5359e7811e333
Hiran Oshadha <128114334+hiranoshadha@users.noreply.github.com>
2025-10-08 12:19:33 +0530

    fixed backend issue

--- webservice/Program.cs
+++ webservice/Program.cs
@@ -1,77 +1,43 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using webservice.data;
-using webservice.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<JwtService>();
-
-//builder.Services.AddSingleton<JwtService>();
 
 builder.Services.AddSingleton<DBConnect>();
+builder.Services.AddScoped<webservice.services.StationService>();
 builder.Services.AddScoped<webservice.services.BookingService>();
 builder.Services.AddScoped<webservice.services.EVOwnerService>();
+
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.Converters.Add(
-            new System.Text.Json.Serialization.JsonStringEnumConverter()
-        );
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
-        policy =>
+        builder =>
         {
-            policy.WithOrigins("http://localhost:5173")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            builder.WithOrigins("http://localhost:5173")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
         });
 });
 
-var jwtSettings = builder.Configuration.GetSection("Jwt");
-var key = Encoding.ASCII.GetBytes(jwtSettings.GetValue<string>("Key"));
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings.GetValue<string>("Issuer"),
-        ValidAudience = jwtSettings.GetValue<string>("Audience"),
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ClockSkew = TimeSpan.Zero
-    };
-});
-
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.MapGet("/", () => "EVCONNECT Backend Started!");
+app.MapControllers();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-app.UseCors("AllowSpecificOrigin");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapGet("/", () => "EVCONNECT Backend Started!");
-app.MapControllers();
 
 app.Run();
